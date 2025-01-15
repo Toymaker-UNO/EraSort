681dcc67e1c0cf32bfb1845e680957a77de9432c
Toymaker <toycraftworld@gmail.com>
2025-01-15 14:48:25 +0900

    Commit EraSort

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,22 +1,99 @@
 #include "Directory.hpp"
-int main(int argc, char* argv[]) {
-    if (argc < 3) {
-        std::cerr << "Usage: " << argv[0] << " <path> <extension1> [<extension2> ... <extensionN>]" << std::endl;
-        return 1;
-    }
 
-    std::string path = argv[1];
+void make_file_list(std::list<std::string>& a_file_list, 
+                   const std::string& a_path, 
+                   const std::list<std::string>& a_extensions) {
+    if (true == era_sort::Directory::is_directory(a_path)) {
+        era_sort::Directory::get_file_list(a_file_list, a_path, a_extensions);
+    }
+}
+
+bool filter(const std::string& a_file_name) {
+    if(std::string::npos != a_file_name.find("/$RECYCLE.BIN/")) {
+        return false;
+    }
+    else if (std::string::npos != a_file_name.find("/boost_")) {
+        return false;
+    }
+    else if (std::string::npos != a_file_name.find("/Program Files/")) {
+        return false;
+    }
+    else if (std::string::npos != a_file_name.find("/Program Files (x86)/")) {
+        return false;
+    }
+    else if (std::string::npos != a_file_name.find("/Windows/")) {
+        return false;
+    }
+    else if (std::string::npos != a_file_name.find("/Emojis/")) {
+        return false;
+    }
+    else if (std::string::npos != a_file_name.find("/Microsoft/")) {
+        return false;
+    }
+    else if (std::string::npos != a_file_name.find("/Office/")) {
+        return false;
+    }
+    else if (std::string::npos != a_file_name.find("AI Essential")) {
+        return false;
+    }
+    else if (std::string::npos != a_file_name.find("/Autodesk/")) {
+        return false;
+    }
+    else if (std::string::npos != a_file_name.find("/.vscode/")) {
+        return false;
+    }
+    else if (std::string::npos != a_file_name.find("/Chrome/")) {
+        return false;
+    }
+    else if (std::string::npos != a_file_name.find("/Python/")) {
+        return false;
+    }
+    else if (std::string::npos != a_file_name.find("/AppData/")) {
+        return false;
+    }
+    return true;
+}
+
+int main(void) {
     std::list<std::string> extensions;
-    for (int i = 2; i < argc; ++i) {
-        extensions.push_back(argv[i]);
-    }
-
+    extensions.push_back(".jpg");
+    extensions.push_back(".jpeg");
+    extensions.push_back(".png");
+    extensions.push_back(".gif");
+    extensions.push_back(".bmp");
+    extensions.push_back(".tiff");
+    extensions.push_back(".tif");
+    extensions.push_back(".svg");
+    extensions.push_back(".mp4");
+    extensions.push_back(".avi");
+    extensions.push_back(".mov");
+    extensions.push_back(".wmv");
+    extensions.push_back(".mkv");
+    extensions.push_back(".flv");
+    
     std::list<std::string> file_list;
-    era_sort::Directory::get_file_list(file_list, path, extensions);
+    make_file_list(file_list, "c:/", extensions);
+    make_file_list(file_list, "d:/", extensions);
+    make_file_list(file_list, "e:/", extensions);
+    make_file_list(file_list, "f:/", extensions);
+    make_file_list(file_list, "g:/", extensions);
+    make_file_list(file_list, "h:/", extensions);
+    make_file_list(file_list, "i:/", extensions);
+    make_file_list(file_list, "j:/", extensions);
+    make_file_list(file_list, "k:/", extensions);
+    make_file_list(file_list, "l:/", extensions);
+    make_file_list(file_list, "m:/", extensions);
+    make_file_list(file_list, "n:/", extensions);
 
+
+    //FILE* p_file = fopen("./file_list.txt", "w+");
     for (const auto& file : file_list) {
-        std::cout << file << std::endl;
+        if(false == filter(file)) {
+            continue;
+        }
+        fprintf(stdout, "%s\n", file.c_str());
     }
+    //fclose(p_file); 
 
     return 0;
 }
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-g++ -W -Wall -I ./hpp ./src/main.cpp -o te.exe
-./te.exe /c/Isla_Sorna/ hpp
-rm -rf ./te.exe
+g++ -W -Wall -I ./hpp ./cpp/main.cpp -o EraSort.exe
+./EraSort.exe
+#rm -rf ./EraSort.exe
